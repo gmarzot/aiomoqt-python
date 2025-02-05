4ce4accf5e0507a78899e8019b580c6083e4f269
gmarzot <gmarzot@marzresearch.net>
2025-02-05 15:43:04 -0500

    add workflow, test stub, pypi support

---
+++ moqt/tests/test_protocol.py
@@ -0,0 +1,9 @@
+import pytest
+from moqt.protocol import MOQTProtocol
+from moqt.client import MOQTClient
+from moqt.types import *
+from moqt.messages import *
+
+
+def test_moqt_stub():
+    assert True
